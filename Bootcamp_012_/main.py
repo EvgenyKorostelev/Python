44875d48d2dc1cec151ce797b3b786c0e45ca1b1
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-12-17 15:14:40 +0300

    add picture in Bootcamp_012_

--- Bootcamp_012_/main.py
+++ Bootcamp_012_/main.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('C:\Users\ddc_s\OneDrive\Рабочий стол\Python\Bootcamp_012_\Joker.jpg')
+img = cv2.imread('C:/Users/ddc_s/OneDrive/Рабочий стол/Python/Bootcamp_012_/Joker.jpg')
 
 #img = cv2.resize(img, (500, 500))
 
--- Bootcamp_012_/video.py
+++ Bootcamp_012_/video.py
@@ -1,19 +1,23 @@
 import cv2
+# репозиторий классификаторы с распознаванием
+# https://github.com/opencv/opencv/tree/4.x/data/haarcascades
 
 face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
+# рамка лица в картинке
 # img = cv2.imread('face.jpg')
 # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # faces = face_cascades.detectMultiScale(img_gray)
 
 # for (x, y, w, h) in faces:
-#     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+#     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
 # cv2.imshow('Result', img)
 # cv2.waitKey(0)
 
+# рамка лица в веб камере
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -22,7 +26,7 @@ while True:
     faces = face_cascades.detectMultiScale(img_gray)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('Result', frame)
 
